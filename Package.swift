56ae4297d78743af06f20984a87ceb8edb5558a1
UKS <skulptorrr@duck.com>
2025-12-10 22:14:58 +0200

    MACOSX_DEPLOYMENT_TARGET = 13.0;

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "STTextView",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v13)],
     products: [
         .library(
             name: "STTextView",
